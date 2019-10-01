d5f63f4ce82d032aa69777b77e159a7130bd3556
finzeance <alfin.zidane@gmail.com>
2019-10-01 08:42:01 +0700

    commit bro

---
+++ AsisPBO2/src/InfoBukuMain.java
@@ -0,0 +1,8 @@
+public class InfoBukuMain {
+
+    public static void main(String[] args) {
+        InfoBuku ib1 = new InfoBuku("Thoriq Afa", "Cara Membobol Database PAPSI", 2018, 1, 60000 );
+        ib1.info();
+    }
+    
+}
